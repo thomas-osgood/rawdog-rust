0e4ee498c2704ac155c5a78afe9d57f28682c4c1
thomas-osgood <thomas-osgood@users.noreply.github.com>
2025-07-03 20:09:22 -0400

    added serde and serde_json

--- src/client/models.rs
+++ src/client/models.rs
@@ -1,4 +1,6 @@
-#[derive(Debug, Clone, PartialEq, Default)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, PartialEq, Default, Deserialize, Serialize)]
 /// struct defining the architecture of General Metadata
 /// that is expected when making a request to a Rawdog Server.
 pub struct GeneralMetadata {
