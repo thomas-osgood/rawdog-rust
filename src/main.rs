11033f7939932a403c95758ce7677f166a7b1439
thomas-osgood <thomas-osgood@users.noreply.github.com>
2025-07-04 00:06:27 -0400

    added another test to main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 use crate::client::models::GeneralMetadata;
 
 mod client;
@@ -6,6 +8,7 @@ fn main() {
     let test_client: client::client::RawdogClient = client::client::RawdogClient::default();
     println!("{:?}", test_client);
 
+    // test of short transmission...
     match test_client.send(
         GeneralMetadata {
             endpoint: 1,
@@ -21,4 +24,26 @@ fn main() {
         }
         Err(e) => println!("ERROR SENDING DATA: {:?}", e),
     };
+
+    // test of longer transmission...
+    match fs::read_to_string("/etc/passwd") {
+        Ok(content) => {
+            match test_client.send(
+                GeneralMetadata {
+                    endpoint: 1,
+                    addldata: "".to_string(),
+                    agentname: "test agent".to_string(),
+                },
+                content,
+            ) {
+                Ok((md, resp)) => {
+                    println!("Data successfully transmitted");
+                    println!("MD: {:?}", md);
+                    println!("DATA: {:?}", resp);
+                }
+                Err(e) => println!("ERROR SENDING DATA: {:?}", e),
+            }
+        }
+        Err(e) => println!("ERROR OPENING FILE: {:?}", e),
+    }
 }
