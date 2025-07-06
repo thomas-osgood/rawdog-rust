d324379117cc0264419e94f95759afe89453bdaa
thomas-osgood <thomas-osgood@users.noreply.github.com>
2025-07-05 23:12:35 -0400

    added has_message func

--- src/client/models.rs
+++ src/client/models.rs
@@ -40,6 +40,16 @@ impl Default for TcpStatusMessage {
 
 impl TcpStatusMessage {
     /// helper function designed to determine if the
+    /// object contains a non-zero length message.
+    ///
+    /// this trims the message field before checking
+    /// the length.
+    pub fn has_message(&self) -> bool {
+        let trimmed: &str = self.message.trim();
+        trimmed.len() > 0
+    }
+
+    /// helper function designed to determine if the
     /// status message contains an error code (400 or higher).
     pub fn is_error(&self) -> bool {
         self.code >= 400
